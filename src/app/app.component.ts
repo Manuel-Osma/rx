0b0e73f7e60c291bbf7a37bf21053a772ed157b0
Jose Manuel Sanchez Osma <Jose.Sanchez@evertecinc.com>
2022-07-26 15:58:53 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PeticionEnvio, ResponseG } from './interfaces/models.interfaces';
+import { ServiceService } from './service/service.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rx';
+  peticion = new PeticionEnvio();
+  peticionPut = new PeticionEnvio();
+  resultado!: Array<ResponseG>;
+  id: number = 1;
+  constructor(private phttp: ServiceService) { }
+
+  onSubmit(tipo: number) {
+    switch ( tipo ) {
+      case 1:
+      this.postSentServices(this.peticion);
+      break;
+      case 2:
+      if(this.peticionPut.author == null && this.peticionPut.title == null){
+        alert("llene todo los campos de put");
+
+      }else{
+        this.putSentServices(this.peticionPut, this.id);
+      }
+      break;
+    }
+}
+onchange($event) {
+  this.id = $event.target.value;
+
+}
+
+
+  getPeticion() {
+    this.phttp.getRespuesta().subscribe(
+      data => {
+        this.resultado = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  postSentServices(body: PeticionEnvio) {
+    this.phttp.postRespuesta(body).subscribe(
+      data => {
+        this.resultado = [];
+        this.resultado.push(data);
+      }
+    )
+
+  }
+  putSentServices(body: PeticionEnvio, id: number) {
+    this.phttp.putRespuesta(id, body).subscribe(
+      data => {
+        this.resultado = [];
+        this.resultado.push(data);
+      }
+    )
+  }
 }
---
+++ src/app/interfaces/models.interfaces.ts
@@ -0,0 +1,10 @@
+export class PeticionEnvio{
+  public title!:string;
+  public  author!:string;
+}
+
+export class ResponseG{
+  public id!:number;
+  public title!:string;
+  public author!: string;
+}
